1428b4e78af008835f0a4538c7275652f9d2c414
projintegrador2023 <127967558+projintegrador2023@users.noreply.github.com>
2023-07-02 22:46:40 -0300

    Update sidebar.js

--- html/js/sidebar.js
+++ html/js/sidebar.js
@@ -1,7 +1,8 @@
 // SIDEBAR TOGGLE
 
-var sidebarOpen = false;
-var sidebar = document.getElementById("sidebar");
+let sidebarOpen = false;
+const sidebar = document.getElementById("sidebar");
+// console.log(sidebar)
 
 function openSidebar() {
   if(!sidebarOpen) {
