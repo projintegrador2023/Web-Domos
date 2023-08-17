e4da711f1e260a6f7cf7ef55259b5f821d1de8c7
projintegrador2023 <127967558+projintegrador2023@users.noreply.github.com>
2023-08-17 12:45:41 -0300

    melhora no design do cadastro de condominio

--- html/cadastro_condominio.php
+++ html/cadastro_condominio.php
@@ -7,12 +7,14 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="shortcut icon" type="image/png" href="css/img/logo.png"/>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous" defer></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
     <script src="js/script.js" defer></script>
+    <script src="js/formulario.js" defer></script>
     <title> Cadastro do condomínio - Domos </title>
     <title>Document</title>
 </head>
 <body>
-    <div class="fundo-imagem">
+    <div class="fundo-imagem pb-5">
         <!-- Cabeçalho da página --> 
         <header class="d-flex justify-content-between p-2">
 
@@ -52,7 +54,7 @@
 
         <div class="col-12">
             <form>
-                <div class="formulario m-auto col-5 p-5 mb-5">
+                <div class="formulario m-auto col-9 col-md-7 col-lg-5 col-xl-4 p-5 mb-5 d-block" id="div_cadastro_condominio">
                     <h1 class="color-0491a3 fw-400 text-center mb-5">Cadastro do Condomínio</h1>
                     <label for="cnpj_condominio" class="text-start color-0491a3 fw-400 fs-4">CNPJ*</label>
                     <input type="text" id="cnpj_condominio" class="col-12 p-2 mb-3 input-form bg-e8e8e8 fs-4 text-black">
@@ -62,10 +64,14 @@
                     <input type="email" id="email_condominio" class="col-12 p-2 mb-3 input-form bg-e8e8e8 fs-4 text-black">
                     <label for="senha_condominio" class="text-start color-0491a3 fw-400 fs-4">Senha*</label>
                     <input type="password" id="senha_condominio" class="col-12 p-2 mb-3 input-form bg-e8e8e8 fs-4 text-black">
+                    <div class="text-end col-12 mt-4">
+                        <button type="button" class="bg-005661 color-fff p-2 rounded border-0 button_formulario col-12 col-md-5 col-xl-4 fs-3 hover-0491a3" onclick="trocar_formulario()"> Continuar </button>
+                    </div>
 
                 </div>
 
-                <div class="formulario m-auto col-5 p-5 mb-5">
+                <div class="formulario m-auto col-9 col-md-7 col-lg-5 col-xl-4 p-5 mb-5 d-none" id="div_cadastro_endereco">
+                <button type="button" class="rounded-5 border-0 fs-1 bg-e8e8e8" onclick="voltar_formulario()"><i class="fa-solid fa-circle-arrow-left color-0491a3"></i></button>
                     <h1 class="color-0491a3 fw-400 text-center mb-5">Endereço</h1>
                     <label for="cep_condominio" class="text-start color-0491a3 fw-400 fs-4 d-block">CEP*</label>
                     <input type="text" id="cep_condominio" class="col-4 input-form bg-e8e8e8 fs-4 p-2 mb-3 text-black d-block">
@@ -90,11 +96,15 @@
                             <input type="text" id="complemento" class="col-12 fs-4 p-2 text-black input-form bg-e8e8e8 d-block">
                         </label>
                     </div>
+                    <div class="text-end col-12 mt-5">
+                        <button type="button" class="bg-005661 color-fff p-2 rounded border-0 button_formulario col-4 fs-3 hover-0491a3" onclick="trocar_formulario()"> Continuar </button>
+                    </div>
                 </div>
 
-                <div class="formulario m-auto col-5 p-5">
+                <div class="formulario m-auto col-9 col-md-7 col-lg-5 col-xl-4 p-5 d-none" id="div_cadastro_informacoes">
+                    <button type="button" class="rounded-5 border-0 fs-1 bg-e8e8e8" onclick="voltar_formulario()"><i class="fa-solid fa-circle-arrow-left color-0491a3"></i></button>
                     <h1 class="color-0491a3 fw-400 text-center mb-5">Informações sobre o condomínio</h1>
-                    <p class="color-0491a3 fs-4 fw-400 col-5 d-block">Faixa de moradores*
+                    <p class="color-0491a3 fs-4 fw-400 col-12 d-block">Faixa de moradores*
                         <select id="faixa_qtd_moradores" class="form-select d-block fs-4 p-2 color-0491a3 col-12 mb-5">
                             <option value="default" class="text-black">Escolha uma opção</option>
                             <option value="0-99" class="text-black">0-100</option>
@@ -114,8 +124,8 @@
                             <label for="cores" class="text-black fs-4">Cores</label><br>
                             <input type="radio" name="divisao" id="letras" class="">
                             <label for="letras" class="text-black fs-4">Letras</label><br>
-                            <input type="radio" name="divisao" id="outro" class="">
-                            <label for="letras" class="text-black fs-4">Nenhum</label><br>
+                            <input type="radio" name="divisao" id="nenhum" class="">
+                            <label for="nenhum" class="text-black fs-4">Nenhum</label><br>
                             <input type="radio" name="divisao" id="outro" class="">  
                             <label for="outro" class="text-black fs-4">Outro:</label>
                             <input type="text" id="outra_divisao" class="input-form bg-e8e8e8 text-black p-0 fs-4">
@@ -137,14 +147,21 @@
                         <textarea class="form-control p-3 fs-4" id="nome_divisao" placeholder="Exemplo: A;B;C;D" rows="3"></textarea>
                     </div>
                     <div class="mb-5">
-                        <label for="nome_divisao" class="form-label color-0491a3 fs-4">Digite os números das casas / apartamentos separados por ponto e vírgula (;)*:</label>
-                        <textarea class="form-control p-3 fs-4" id="nome_divisao" placeholder="Exemplo: 101;102;103;104;201;202;203;204" rows="3"></textarea>
+                        <label for="numero_casa" class="form-label color-0491a3 fs-4">Digite os números das casas / apartamentos separados por ponto e vírgula (;)*:</label>
+                        <textarea class="form-control p-3 fs-4" id="numero_casa" placeholder="Exemplo: 101;102;103;104;201;202;203;204" rows="3"></textarea>
                     </div>
                     <div class="mb-5">
                       <label for="" class="form-label color-0491a3 fs-4">Insira o pdf de regimento interno (opcional):</label>
                       <input type="file" class="form-control" id="regimento_interno">
                     </div>
-                    <h3 class="color-0491a3 text-center fs-3 mb-4">Informações dos espaços públicos (opcional)</h3>
+                    <div class="text-end col-12 mt-5">
+                        <button type="button" class="bg-005661 color-fff p-2 rounded border-0 button_formulario col-4 fs-3 hover-0491a3" onclick="trocar_formulario()"> Continuar </button>
+                    </div>
+                </div>
+
+                <div class="formulario m-auto col-9 col-md-7 col-lg-5 col-xl-4 p-5 d-none" id="div_cadastro_informacoes_opcionais">
+                    <button type="button" class="rounded-5 border-0 fs-1 bg-e8e8e8" onclick="voltar_formulario()"><i class="fa-solid fa-circle-arrow-left color-0491a3"></i></button>
+                    <h1 class="color-0491a3 text-center fs-3 mb-4">Informações dos espaços públicos (opcional)</h1>
                     <div class="form-check mb-5">
                         <input class="form-check-input" type="checkbox" value="saloes_de_festas" id="saloes_de_festas">
                         <label class="form-check-label color-0491a3 fs-4" for="saloes_de_festas">
@@ -197,7 +214,7 @@
                     </div>
 
                     <div class="text-end col-12">
-                        <button class="bg-005661 color-fff p-2 rounded border-0 button_formulario col-5 fs-3 hover-0491a3"> Cadastrar </button>
+                        <button class="bg-005661 color-fff p-2 rounded border-0 button_formulario col-4 fs-3 hover-0491a3"> Cadastrar </button>
                     </div>
                 </div>
             </form>
