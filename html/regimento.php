e978608b806a9a2be9c74ce78a37b3befe21476b
projintegrador2023 <127967558+projintegrador2023@users.noreply.github.com>
2023-08-22 10:58:23 -0300

    embed pdf do regimento

--- html/regimento.php
+++ html/regimento.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
     <link rel="stylesheet" href="css/sidebar.css">
     <link rel="stylesheet" href="css/regimento.css"> 
+    <link rel="stylesheet" href="css/css_davi.css">
     <link rel="shortcut icon" type="image/png" href="css/img/logo.png"/>
     <title> Regimento - Domos </title>
 </head>
@@ -60,68 +61,10 @@
         <main class="main-container"> 
             
             <!-- Div contendo o card--> 
-            <div class="card tamanho-geral m-2">
-            <div class="card-body">
-                <h5 class="card-title titulo"> Regimento Interno do Condomínio Residencial Vila Serena </h5>
-                <h6 class="card-subtitle mb-2 titulo-sub">Leia atentamente o regimento interno. Descumprimento de normas passível a multa.</h6>
-                <p class="card-text descricao">
-
-                    Capítulo I: Disposições Gerais <br>
-                    
-                    Artigo 1: Este Regimento Interno estabelece as normas e diretrizes para a convivência harmoniosa e organizada dos moradores, proprietários, inquilinos e demais usuários do Condomínio Residencial "Vila Serena", doravante denominado "Condomínio". <br>
-                    
-                    Artigo 2: O presente Regimento Interno é de aplicação obrigatória a todos os condôminos, inquilinos, residentes e seus respectivos visitantes. <br>
-                    
-                    Artigo 3: O objetivo deste Regimento é preservar a segurança, tranquilidade, ordem e bem-estar de todos os moradores, buscando garantir uma convivência harmoniosa e a preservação das áreas comuns e privativas do condomínio. <br>
-                    
-                    Capítulo II: Direitos e Deveres dos Moradores <br>
-                    
-                    Artigo 4: Todos os moradores têm o direito de usufruir das áreas comuns do condomínio, desde que respeitem as normas estabelecidas neste Regimento. <br>
-                    
-                    Artigo 5: É dever de todos os moradores manter seus apartamentos ou casas em condições adequadas de higiene, limpeza e conservação, de forma a evitar a proliferação de pragas e odores desagradáveis. <br>
-                    
-                    Artigo 6: É dever dos moradores respeitar o horário de silêncio estabelecido pelo condomínio, evitando ruídos excessivos que possam perturbar o sossego dos demais moradores, especialmente no período das 22h às 8h. <br>
-                    
-                    Artigo 7: É proibido o uso das áreas comuns para fins comerciais ou atividades que possam causar transtornos aos demais moradores.<br>
-                    
-                    Artigo 8: É obrigatório o uso adequado dos equipamentos e instalações do condomínio, como elevadores, escadas, portões, interfone, academia, salão de festas, entre outros, sendo responsabilidade dos moradores zelar pela sua conservação e não praticar atos que possam danificá-los.<br>
-                    
-                    Capítulo III: Uso das Áreas Comuns<br>
-                    
-                    Artigo 9: O uso das áreas comuns do condomínio deverá obedecer às regras estabelecidas pela administração, incluindo horários de funcionamento, reserva de espaços e a devida limpeza após o uso.<br>
-                    
-                    Artigo 10: É proibido o consumo de bebidas alcoólicas e a prática de atividades que possam causar danos ou perturbar a tranquilidade dos demais moradores nas áreas comuns, a menos que haja autorização expressa da administração.<br>
-                    
-                    Artigo 11: Crianças menores de 12 anos devem ser supervisionadas por um adulto responsável ao utilizarem as áreas comuns do condomínio.<br>
-                    
-                    Artigo 12: É proibido o uso de churrasqueiras, piscinas e demais equipamentos de lazer sem a devida autorização e agendamento prévio.<br>
-                    
-                    Capítulo IV: Segurança e Convivência <br>
-
-                    Artigo 13: É obrigatório o uso dos dispositivos de segurança, como identificação para acesso ao condomínio, cartões de acesso e controle de portões, a fim de garantir a segurança de todos os moradores. <br>
-
-                    Artigo 14: É proibido o acesso de pessoas estranhas ao condomínio sem a devida autorização dos moradores ou da administração. <br>
-
-                    Artigo 15: É proibido o estacionamento em locais não autorizados ou em vagas destinadas a outros moradores, salvo em casos de emergência devidamente comunicados à administração.<br>
-
-                    Artigo 16: É obrigatório o descarte correto de lixo e materiais recicláveis nos locais indicados para essa finalidade, contribuindo para a limpeza e preservação do condomínio.<br>
-
-                    Capítulo V: Penalidades<br>
-
-                    Artigo 17: O descumprimento das normas estabelecidas neste Regimento poderá acarretar sanções disciplinares, conforme determinado pela administração do condomínio, que poderão incluir advertências verbais ou escritas, multas, suspensão de direitos e, em casos mais graves, a aplicação de medidas judiciais cabíveis.<br>
-
-                    Artigo 18: As sanções disciplinares serão aplicadas de acordo com a gravidade da infração e poderão ser estabelecidas de forma progressiva, considerando reincidências ou ocorrências recorrentes.<br>
-
-                    Capítulo VI: Disposições Finais<br>
-
-                    Artigo 19: Qualquer alteração ou revisão deste Regimento Interno deverá ser aprovada pela maioria dos condôminos em assembleia geral, convocada especificamente para esse fim.<br>
-
-                    Artigo 20: Os casos omissos neste Regimento serão analisados e decididos pela administração do condomínio, levando em consideração os princípios de convivência harmoniosa e o bem-estar coletivo.<br>
-
-                    Artigo 21: Este Regimento entra em vigor a partir da data de sua aprovação e deverá ser afixado em local de fácil acesso a todos os moradores.
-                                        
-                </p>
-            </div>
+            <div class="card m-2 h-100 shadow-black">
+                <div class="h-100">
+                    <embed src="teste.pdf" class="col-12 h-100" type='application/pdf'>
+                </div>
             </div>    
         </main>
     </div>
