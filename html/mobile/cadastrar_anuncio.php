3076593fc609b94f9062dcda8a7ac1811bfa3da6
projintegrador2023 <127967558+projintegrador2023@users.noreply.github.com>
2023-10-17 10:55:42 -0300

    começo ligação com bd

---
+++ html/mobile/cadastrar_anuncio.php
@@ -0,0 +1,108 @@
+<?php
+ 
+/*
+ * O seguinte codigo abre uma conexao com o BD e adiciona um produto nele.
+ * As informacoes de um produto sao recebidas atraves de uma requisicao POST.
+ */
+
+// conexão com bd
+require_once('conexao_db.php');
+
+// autenticação
+require_once('autenticacao.php');
+
+// array de resposta
+$resposta = array();
+
+//fuso horario de sao paulo
+$timezone = new DateTimeZone('America/Sao_Paulo');
+
+// verifica se o usuário conseguiu autenticar
+if(autenticar($db_con)) {
+	
+	// Primeiro, verifica-se se todos os parametros foram enviados pelo cliente.
+	// A criacao de um produto precisa dos seguintes parametros:
+	// nome - nome do produto
+	// preco - preco do produto
+	// descricao - descricao do produto
+	// img - imagem do produto
+	if (isset($_POST['descricao']) && isset($_POST['titulo'])  && isset($_POST['tag']) && isset($_FILES['img'])) {
+		
+		// Aqui sao obtidos os parametros
+		$descricao = $_POST['descricao'];
+		$titulo = $_POST['titulo'];
+		$tag = $_POST['tag'];
+		//pega a hora do post no fuso horario de sao paulo
+		$data_hora_postagem = new DateTime('now', $timezone);
+		
+		$filename = $_FILES['img']['tmp_name'];
+		$client_id="ce5d3a656e2aa51"; //mudar pra nossa conta do imgur
+		$handle = fopen($filename, "r");
+		$data = fread($handle, filesize($filename));
+		$pvars   = array('image' => base64_encode($data));
+		$timeout = 30;
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, 'https://api.imgur.com/3/image.json');
+		curl_setopt($curl, CURLOPT_TIMEOUT, $timeout);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Client-ID ' . $client_id));
+		curl_setopt($curl, CURLOPT_POST, 1);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $pvars);
+		$out = curl_exec($curl);
+		curl_close ($curl);
+		$pms = json_decode($out,true);
+		$img_url=$pms['data']['link'];
+		
+		
+		// A proxima linha insere um novo produto no BD.
+		// A variavel consulta indica se a insercao foi feita corretamente ou nao.
+		$consulta = $db_con->prepare("INSERT INTO IMAGEM(link) VALUES('$img_url')");
+		if ($consulta->execute()) {
+			// Se o produto foi inserido corretamente no servidor, o cliente 
+			// recebe a chave "sucesso" com valor 1
+			$resposta["sucesso"] = 1;
+			
+		} else {
+			// Se o produto nao foi inserido corretamente no servidor, o cliente 
+			// recebe a chave "sucesso" com valor 0. A chave "erro" indica o 
+			// motivo da falha.
+			$resposta["sucesso"] = 0;
+			$resposta["erro"] = "Erro ao salvar imagem no BD: " . $consulta->error;
+		}
+
+		// A proxima linha insere um novo produto no BD.
+		// A variavel consulta indica se a insercao foi feita corretamente ou nao.
+		$consulta = $db_con->prepare("INSERT INTO POSTAGEM(data_hora_postagem, descricao , titulo , FK_USUARIO_cpf) VALUES('$data_hora_postagem', '$descricao', '$titulo', '$login')");
+		if ($consulta->execute()) {
+			// Se o produto foi inserido corretamente no servidor, o cliente 
+			// recebe a chave "sucesso" com valor 1
+			$resposta["sucesso"] = 1;
+			
+		} else {
+			// Se o produto nao foi inserido corretamente no servidor, o cliente 
+			// recebe a chave "sucesso" com valor 0. A chave "erro" indica o 
+			// motivo da falha.
+			$resposta["sucesso"] = 0;
+			$resposta["erro"] = "Erro ao criar produto no BD: " . $consulta->error;
+		}
+	} else {
+		// Se a requisicao foi feita incorretamente, ou seja, os parametros 
+		// nao foram enviados corretamente para o servidor, o cliente 
+		// recebe a chave "sucesso" com valor 0. A chave "erro" indica o 
+		// motivo da falha.
+		$resposta["sucesso"] = 0;
+		$resposta["erro"] = "Campo requerido nao preenchido";
+	}
+}
+else {
+	// senha ou usuario nao confere
+	$resposta["sucesso"] = 0;
+	$resposta["erro"] = "usuario ou senha não confere";
+}
+
+// Fecha a conexao com o BD
+$db_con = null;
+
+// Converte a resposta para o formato JSON.
+echo json_encode($resposta);
+?>
