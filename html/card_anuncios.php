94b2d2f1a972afa04ea52925a75889c262191228
projintegrador2023 <127967558+projintegrador2023@users.noreply.github.com>
2023-11-21 09:09:48 -0300

    include de avisos pesquisa nao

--- html/card_anuncios.php
+++ html/card_anuncios.php
@@ -1,31 +1,35 @@
-<!-- INICIO CARD  -->
-<div class="card m-2 p-0 col-lg-5 col-xl-3 col-8">
-    <div class="card-header bg-transparent">
+<?php 
+    echo '
+    <!-- INICIO CARD  -->
+    <div class="card m-2 p-0 col-lg-5 col-xl-3 col-8">
+      <div class="card-header bg-transparent">
         <div class="d-flex">
-        <h4 class="flex-grow-1 sticky-top color-titulo card-title">Nome Morador</h4>
-        <!--Menu dropdown com funcionalidades que serão adicionadas posteriormente-->
+          <h4 class="flex-grow-1 sticky-top color-titulo card-title">Nome Morador</h4>
+          <!--Menu dropdown com funcionalidades que serão adicionadas posteriormente-->
             <div class="d-flex sticky-top">
-            <button id="salvo" class="btn"><i class="fa-regular fa-bookmark color-0491a3" onclick="salvos()" estado="vazia"></i><i class="fa-solid fa-bookmark color-0491a3"  style="display: none;" estado="cheia"></i></button>
-            <button class="btn" type="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-ellipsis-vertical text-dark"></i></button>
-            <div class="dropdown">
+              <button id="salvo" class="btn"><i class="fa-regular fa-bookmark color-0491a3" onclick="salvos()" estado="vazia"></i><i class="fa-solid fa-bookmark color-0491a3"  style="display: none;" estado="cheia"></i></button>
+              <button class="btn" type="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-ellipsis-vertical text-dark"></i></button>
+              <div class="dropdown">
                 <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Excluir</a></li>
+                  <li><a class="dropdown-item" href="#">Excluir</a></li>
                 </ul>
+              </div>
             </div>
-            </div>
-        </div> 
+          </div> 
         <div>
-        <h6 class="card-subtitle color-subtitulo">Apto num - numbloco</h6>
+          <h6 class="card-subtitle color-subtitulo">Apto num - numbloco</h6>
         </div>
-    </div>
-    <div class="card-body d-flex row text-success">
+      </div>
+      <div class="card-body d-flex row text-success">
         <h6 class="card-subtitle fs-5 color-titulo">Palha italiana</h6>
         <p class="card-text fs-9 text-justify color-descricao">Feita com chocolate de qualidade e leite condensado cremoso, 
-        nossa palha italiana é perfeita para adoçar seu dia. Peça já a sua e surpreenda-se com essa maravilha italiana. 
-        Pedidos: (27)999656552
+          nossa palha italiana é perfeita para adoçar seu dia. Peça já a sua e surpreenda-se com essa maravilha italiana. 
+          Pedidos: (27)999656552
         </p>
         <img src="css/img/palha_italiana.png" class="w-100 h-50 col-lg-5 col-xl-3 col-8"> 
-    </div>
-    <div class="card-footer btn-alimentacao"></div>
-</div> 
+      </div>
+      <div class="card-footer btn-alimentacao"></div>
+    </div> 
     <!-- FIM DO CARD -->
+    '
+?>
--- html/card_aviso.php
+++ html/card_aviso.php
@@ -1,58 +1,34 @@
-<!--CARD AVISO IGUAL ANUNCIOS-->
-<div class="card m-2 p-0 col-lg-5 col-xl-3 col-8">
-    <div class="card-header bg-transparent">
-        <div class="fs-5 color-titulo d-flex">
-            <!--div class="flex-grow-1"><p class="color-titulo"></p></div> <Usuario-->
-            <!--Menu dropdown com funcionalidades que serão adicionadas posteriormente-->
-            <div class="dropdown">
-                <button class="btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <i class="fa-solid fa-ellipsis-vertical text-dark"></i>
-                </button>
-                
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Editar</a></li>
-                    <li><a class="dropdown-item" href="#">Excluir</a></li>
-                </ul>
-            </div>
-                <button id="salvo" class="btn mb-2"><i class="fa-regular fa-bookmark color-0491a3" onclick="salvos()" estado="vazia"></i><i class="fa-solid fa-bookmark color-0491a3"  style="display: none;" estado="cheia"></i></button>
-        </div>
-    </div>
-            
-    <div class="card-body d-flex row text-success">
-        <h6 class="titulo card-subtitle fs-5 color-titulo"></h6> <!--titulo-->
-        <p class="descricao card-text fs-9 text-justify color-descricao"> </p> <!--descricao-->
-    </div>
-    
-    <div class="filtro card-footer"></div>
 
-<!--CARD AVISO teste teste-->
 <!-- INICIO DO CARD -->
-<div class="card m-2 p-0 col-lg-5 col-xl-3 col-8">
+<?php
+    echo '
+    <div class="card m-2 p-0 col-lg-5 col-xl-3 col-8">
     <div class="card-header bg-transparent">
-    <div class="d-flex">
+      <div class="d-flex">
         <h4 class="flex-grow-1 sticky-top color-titulo card-title">Pia quebrada</h4>
         <!--Menu dropdown com funcionalidades que serão adicionadas posteriormente-->
-        <div class="d-flex sticky-top">
+          <div class="d-flex sticky-top">
             <button id="salvo" class="btn"><i class="fa-regular fa-bookmark color-0491a3" onclick="salvos()" estado="vazia"></i><i class="fa-solid fa-bookmark color-0491a3"  style="display: none;" estado="cheia"></i></button>
             <button class="btn" type="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-ellipsis-vertical text-dark"></i></button>
             <div class="dropdown">
-            <ul class="dropdown-menu">
+              <ul class="dropdown-menu">
                 <li><a class="dropdown-item" href="#">Editar</a></li>
                 <li><a class="dropdown-item" href="#">Excluir</a></li>
-            </ul>
+              </ul>
             </div>
-        </div>
+          </div>
         </div> 
-    <div>
+      <div>
         <h6 class="card-subtitle color-subtitulo">18/06/2023 - 14:31</h6>
-    </div>
+      </div>
     </div>
 
     <div class="card-body text-success pt-2">
-    <p class="card-text fs-9 text-justify color-descricao">Aviso importante aos moradores do condomínio: Informamos que a pia da área comum encontra-se quebrada. 
+      <p class="card-text fs-9 text-justify color-descricao">Aviso importante aos moradores do condomínio: Informamos que a pia da área comum encontra-se quebrada. 
         Pedimos a todos que evitem utilizar o local temporariamente até que a manutenção seja concluída. 
         Contamos com a compreensão e colaboração de todos para a resolução rápida desse inconveniente. Obrigado.</p>
     </div>
     <div class="card-footer btn-critico"></div>
-</div>
-        <!-- FIM DO CARD -->
+  </div>'
+?>
+<!-- FIM DO CARD -->
