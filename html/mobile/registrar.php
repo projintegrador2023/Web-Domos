8099843cfcb12b89f6c6d2547f40cf57ff8a3799
projintegrador2023 <127967558+projintegrador2023@users.noreply.github.com>
2023-11-30 16:42:34 -0300

    Update registrar.php

--- html/mobile/registrar.php
+++ html/mobile/registrar.php
@@ -25,9 +25,9 @@ if (isset($_POST['cpf']) && isset($_POST['nome']) && isset($_POST['email']) && i
 	// um código hash que é gerado a partir da senha.
 	$token = password_hash($senha, PASSWORD_DEFAULT);
 	
-	$sql_bloco = $db_con->prepare("SELECT codigo_divisao FROM DIVISAO WHERE desc_divisao = $divisao");
+	$sql_bloco = $db_con->prepare("SELECT codigo_divisao FROM DIVISAO WHERE desc_divisao = '$divisao'");
 	$sql_bloco->execute();
-	$sql_moradia = $db_con->prepare("SELECT codigo_moradia FROM MORADIA WHERE numero_moradia = $numero_apartamento AND fk_divisao_codigo_divisao = $sql_bloco");
+	$sql_moradia = $db_con->prepare("SELECT codigo_moradia FROM MORADIA WHERE numero_moradia = '$numero_apartamento' AND fk_divisao_codigo_divisao = '$sql_bloco'");
 	$sql_moradia->execute();
 	
 	// antes de registrar o novo usuário, verificamos se ele já
