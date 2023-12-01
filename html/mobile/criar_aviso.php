f951d02f2411fead2630ef19fd711387c212c7d5
projintegrador2023 <127967558+projintegrador2023@users.noreply.github.com>
2023-12-01 10:19:22 -0300

    Update and rename criar_produto.php to criar_aviso.php

--- html/mobile/criar_aviso.php
+++ html/mobile/criar_aviso.php
@@ -17,41 +17,17 @@ $resposta = array();
 // verifica se o usuário conseguiu autenticar
 if(autenticar($db_con)) {
 	
-	// Primeiro, verifica-se se todos os parametros foram enviados pelo cliente.
-	// A criacao de um produto precisa dos seguintes parametros:
-	// nome - nome do produto
-	// preco - preco do produto
-	// descricao - descricao do produto
-	// img - imagem do produto
-	if (isset($_POST['nome']) && isset($_POST['preco']) && isset($_POST['descricao']) && isset($_FILES['img'])) {
+	if (isset($_POST['titulo']) && isset($_POST['importancia']) && isset($_POST['descricao'])) {
 		
 		// Aqui sao obtidos os parametros
-		$nome = $_POST['nome'];
-		$preco = $_POST['preco'];
+		$titulo = $_POST['titulo'];
+		$importancia = $_POST['importancia'];
 		$descricao = $_POST['descricao'];
-		
-		$filename = $_FILES['img']['tmp_name'];
-		$client_id="ce5d3a656e2aa51";
-		$handle = fopen($filename, "r");
-		$data = fread($handle, filesize($filename));
-		$pvars   = array('image' => base64_encode($data));
-		$timeout = 30;
-		$curl = curl_init();
-		curl_setopt($curl, CURLOPT_URL, 'https://api.imgur.com/3/image.json');
-		curl_setopt($curl, CURLOPT_TIMEOUT, $timeout);
-		curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Client-ID ' . $client_id));
-		curl_setopt($curl, CURLOPT_POST, 1);
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($curl, CURLOPT_POSTFIELDS, $pvars);
-		$out = curl_exec($curl);
-		curl_close ($curl);
-		$pms = json_decode($out,true);
-		$img_url=$pms['data']['link'];
-		
 
 		// A proxima linha insere um novo produto no BD.
 		// A variavel consulta indica se a insercao foi feita corretamente ou nao.
-		$consulta = $db_con->prepare("INSERT INTO produtos(nome, preco, descricao, img, usuarios_login) VALUES('$nome', '$preco', '$descricao', '$img_url', '$login')");
+		
+		$consulta = $db_con->prepare("INSERT INTO aviso(data_hora_postagem, descricao, titulo, fk_usuario_cpf, fk_importancia_codigo_importancia, fk_condominio_codigo_condominio) VALUES('$nome', '$descricao', '$titulo', )");
 		if ($consulta->execute()) {
 			// Se o produto foi inserido corretamente no servidor, o cliente 
 			// recebe a chave "sucesso" com valor 1
