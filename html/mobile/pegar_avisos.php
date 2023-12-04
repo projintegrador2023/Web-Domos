55b76208f01e25a6389128bd1790d414942a0685
projintegrador2023 <127967558+projintegrador2023@users.noreply.github.com>
2023-12-04 14:18:16 -0300

    Update pegar_avisos.php

--- html/mobile/pegar_avisos.php
+++ html/mobile/pegar_avisos.php
@@ -1,1 +1,50 @@
+<?php
+ 
+/*
+ * O codigo seguinte retorna os dados detalhados de um produto.
+ * Essa e uma requisicao do tipo GET. Um produto e identificado 
+ * pelo campo id.
+ */
 
+// conexão com bd
+require_once('conexao_db.php');
+
+// array de resposta
+$resposta = array();
+$resposta["avisos"] = array();
+
+$consulta = $db_con->prepare("SELECT * FROM aviso where fk_condominio_codigo_condominio = '$codigo_condominio'");
+
+if ($consulta->execute()) {
+  while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
+    $aviso = array();
+    $aviso["data_hora_postagem"] = $linha["data_hora_postagem"];
+    $aviso["titulo"] = $linha["titulo"];
+    
+    $aviso["cpf"] = $linha["fk_usuario_cpf"];
+    
+    $consulta1 = $db_con->prepare("SELECT desc_importancia FROM importancia where codigo_importancia = '$linha["fk_importancia_codigo_importancia"]'");
+    $consulta1->execute();
+    $linha1 = $consulta->fetch(PDO::FETCH_ASSOC);
+    
+    $aviso["importancia"] = $linha1["desc_importancia"];
+   
+    // Adiciona o produto no array de produtos.
+    array_push($resposta["avisos"], $aviso);
+  }
+  
+  $resposta["sucesso"] = 1;
+} else {
+    // Caso ocorra falha no BD, o cliente 
+    // recebe a chave "sucesso" com valor 0. A chave "erro" indica o 
+    // motivo da falha.
+    $resposta["sucesso"] = 0;
+    $resposta["erro"] = "Erro no BD: " . $consulta->error;
+}
+
+// Fecha a conexao com o BD
+$db_con = null;
+
+// Converte a resposta para o formato JSON.
+echo json_encode($resposta);
+?>
