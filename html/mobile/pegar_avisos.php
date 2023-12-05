e5fa7417532c4a00cfa7bebc222a7be1d931795c
projintegrador2023 <127967558+projintegrador2023@users.noreply.github.com>
2023-12-05 10:48:57 -0300

    Update pegar_avisos.php

--- html/mobile/pegar_avisos.php
+++ html/mobile/pegar_avisos.php
@@ -16,46 +16,47 @@ $resposta["avisos"] = array();
 
 // verifica se o usuário conseguiu autenticar
 if(autenticar($db_con)) {
- if (isset($_GET['limit']) && isset($_GET['offset']) && isset($_GET['cpf']) && isset($_GET['importancia'])) {
-	 
-  $limit = $_GET['limit'];
-  $offset = $_GET['offset'];
-  $cpf = $_GET['cpf'];
-  $importancia = $_GET['importancia'];
-
-  $consulta1 = $db_con->prepare("SELECT codigo_condominio FROM usuario where cpf = '$cpf'");
-  $consulta1->execute();
-  $linha1 = $consulta1->fetch(PDO::FETCH_ASSOC);
-  $codigo_condominio = $linha1['codigo_condominio'];
-
-  $consulta2 = $db_con->prepare("SELECT codigo_importancia FROM importancia where desc_importancia = '$importancia'");
-  $consulta2->execute();
-  $linha2 = $consulta2->fetch(PDO::FETCH_ASSOC);
-  $codigo_importancia = $linha2['codigo_importancia'];
-
-$consulta = $db_con->prepare("SELECT * FROM aviso where fk_condominio_codigo_condominio = '$codigo_condominio' AND fk_importancia_codigo_importancia = '$codigo_importancia'");
-
-if ($consulta->execute()) {
-  while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
-    $aviso = array();
-    $aviso["codigo_postagem"] = $linha["codigo_postagem"];
-    $aviso["data_hora_postagem"] = $linha["data_hora_postagem"];
-    $aviso["titulo"] = $linha["titulo"];
-    $aviso["descricao"] = $linha["descricao"];
-    $aviso["cpf"] = $linha["fk_usuario_cpf"];
-    $aviso["importancia"] = $importancia;
-   
-    // Adiciona o produto no array de produtos.
-    array_push($resposta["avisos"], $aviso);
-  }
-  
-  $resposta["sucesso"] = 1;
-} else {
-    // Caso ocorra falha no BD, o cliente 
-    // recebe a chave "sucesso" com valor 0. A chave "erro" indica o 
-    // motivo da falha.
-    $resposta["sucesso"] = 0;
-    $resposta["erro"] = "Erro no BD: " . $consulta->error;
+	 if (isset($_GET['limit']) && isset($_GET['offset']) && isset($_GET['cpf']) && isset($_GET['importancia'])) {
+		  $limit = $_GET['limit'];
+		  $offset = $_GET['offset'];
+		  $cpf = $_GET['cpf'];
+		  $importancia = $_GET['importancia'];
+		
+		  $consulta1 = $db_con->prepare("SELECT codigo_condominio FROM usuario where cpf = '$cpf'");
+		  $consulta1->execute();
+		  $linha1 = $consulta1->fetch(PDO::FETCH_ASSOC);
+		  $codigo_condominio = $linha1['codigo_condominio'];
+		
+		  $consulta2 = $db_con->prepare("SELECT codigo_importancia FROM importancia where desc_importancia = '$importancia'");
+		  $consulta2->execute();
+		  $linha2 = $consulta2->fetch(PDO::FETCH_ASSOC);
+		  $codigo_importancia = $linha2['codigo_importancia'];
+	
+		$consulta = $db_con->prepare("SELECT * FROM aviso where fk_condominio_codigo_condominio = '$codigo_condominio' AND fk_importancia_codigo_importancia = '$codigo_importancia'");
+	
+		if ($consulta->execute()) {
+	  		while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
+	    		    $aviso = array();
+			    $aviso["codigo_postagem"] = $linha["codigo_postagem"];
+			    $aviso["data_hora_postagem"] = $linha["data_hora_postagem"];
+			    $aviso["titulo"] = $linha["titulo"];
+			    $aviso["descricao"] = $linha["descricao"];
+			    $aviso["cpf"] = $linha["fk_usuario_cpf"];
+			    $aviso["importancia"] = $importancia;
+	   
+			    // Adiciona o produto no array de produtos.
+			    array_push($resposta["avisos"], $aviso);
+	  		}
+	  
+	  		$resposta["sucesso"] = 1;
+		} else {
+		    // Caso ocorra falha no BD, o cliente 
+		    // recebe a chave "sucesso" com valor 0. A chave "erro" indica o 
+		    // motivo da falha.
+		    $resposta["sucesso"] = 0;
+		    $resposta["erro"] = "Erro no BD: " . $consulta->error;
+		}
+	}
 }
 
 // Fecha a conexao com o BD
