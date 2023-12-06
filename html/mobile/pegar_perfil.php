8f1d2afbb008f66eb1c2ef237d7b2d009e8170a9
projintegrador2023 <127967558+projintegrador2023@users.noreply.github.com>
2023-12-06 16:53:19 -0300

    Update pegar_perfil.php

--- html/mobile/pegar_perfil.php
+++ html/mobile/pegar_perfil.php
@@ -11,14 +11,12 @@ require_once('conexao_db.php');
 require_once('autenticacao.php');
 
 // array de resposta
-$resposta = array();
-$resposta["perfil"] = array();
+$perfil = array();
 
 $consulta = $db_con->prepare("SELECT * FROM usuario where cpf = '$cpf'");
 
 if ($consulta->execute()) {
-  while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
-    $perfil = array();
+  $linha = $consulta->fetch(PDO::FETCH_ASSOC);
     $perfil["cpf"] = $linha["cpf"];
     $perfil["nome"] = $linha["nome"];
     $perfil["email"] = $linha["email"];
@@ -42,21 +40,21 @@ if ($consulta->execute()) {
     $perfil["divisao"] = $linha2["desc_divisao"];
    
     // Adiciona o produto no array de produtos.
-    array_push($resposta["perfil"], $perfil);
+   
   }
   
-  $resposta["sucesso"] = 1;
+  $perfil["sucesso"] = 1;
 } else {
     // Caso ocorra falha no BD, o cliente 
     // recebe a chave "sucesso" com valor 0. A chave "erro" indica o 
     // motivo da falha.
-    $resposta["sucesso"] = 0;
-    $resposta["erro"] = "Erro no BD: " . $consulta->error;
+    $perfil["sucesso"] = 0;
+    $perfil["erro"] = "Erro no BD: " . $consulta->error;
 }
 
 // Fecha a conexao com o BD
 $db_con = null;
 
 // Converte a resposta para o formato JSON.
-echo json_encode($resposta);
+echo json_encode($perfil);
 ?>
