2e9e4a697c9e4c51daa6ec912a7f114847fe4c0d
projintegrador2023 <127967558+projintegrador2023@users.noreply.github.com>
2023-12-06 15:58:54 -0300

    Update pegar_perfil.php

--- html/mobile/pegar_perfil.php
+++ html/mobile/pegar_perfil.php
@@ -11,8 +11,7 @@ require_once('conexao_db.php');
 require_once('autenticacao.php');
 
 // array de resposta
-$resposta = array();
-$resposta["perfil"] = array();
+$perfil = array();
 
 if(autenticar($db_con)) {
   if (isset($_GET['cpf'])) {
@@ -20,17 +19,14 @@ if(autenticar($db_con)) {
 $consulta = $db_con->prepare("SELECT * FROM usuario where cpf = '$cpf'");
 
 if ($consulta->execute()) {
-  while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
-    $perfil = array();
+  $linha = $consulta->fetch(PDO::FETCH_ASSOC);
     $perfil["cpf"] = $linha["cpf"];
     $perfil["nome"] = $linha["nome"];
     $perfil["email"] = $linha["email"];
-    $perfil["codigo_nivel_permissao"] = $linha["fk_nivel_permissao_codigo_nivel_permissao"];
-    $perfil["codigo_condominio"] = $linha["fk_condominio_codigo_condominio"];
-    $perfil["senha"] = $linha["senha"];
+ 
    $codigo_moradia = $linha["fk_moradia_codigo_moradia"];
     
-    $consulta1 = $db_con->prepare("SELECT * FROM moradia where codigo_moradia = $codigo_moradia");
+    $consulta1 = $db_con->prepare("SELECT * FROM moradia where codigo_moradia = '$codigo_moradia'");
     $consulta1->execute();
     $linha1 = $consulta->fetch(PDO::FETCH_ASSOC);
     
@@ -38,28 +34,23 @@ if ($consulta->execute()) {
 
    $codigo_divisao = $linha1["fk_divisao_codigo_divisao"];
 
-    $consulta2 = $db_con->prepare("SELECT * FROM divisao where codigo_divisao = $codigo_divisao");
+    $consulta2 = $db_con->prepare("SELECT * FROM divisao where codigo_divisao = '$codigo_divisao'");
     $consulta2->execute();
     $linha2 = $consulta->fetch(PDO::FETCH_ASSOC);
     
     $perfil["divisao"] = $linha2["desc_divisao"];
-   
-    // Adiciona o produto no array de produtos.
-    array_push($resposta["perfil"], $perfil);
-  }
-  
-  $resposta["sucesso"] = 1;
+  $perfil["sucesso"] = 1;
 } else {
     // Caso ocorra falha no BD, o cliente 
     // recebe a chave "sucesso" com valor 0. A chave "erro" indica o 
     // motivo da falha.
-    $resposta["sucesso"] = 0;
-    $resposta["erro"] = "Erro no BD: " . $consulta->error;
+    $perfil["sucesso"] = 0;
+    $perfil["erro"] = "Erro no BD: " . $consulta->error;
 }
   }}
 // Fecha a conexao com o BD
 $db_con = null;
 
 // Converte a resposta para o formato JSON.
-echo json_encode($resposta);
+echo json_encode($perfil);
 ?>
