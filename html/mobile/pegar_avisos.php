d7a174763e28bdee53eb893775272360961de554
projintegrador2023 <127967558+projintegrador2023@users.noreply.github.com>
2023-12-06 14:00:09 -0300

    Update pegar_avisos.php

--- html/mobile/pegar_avisos.php
+++ html/mobile/pegar_avisos.php
@@ -26,13 +26,22 @@ if(autenticar($db_con)) {
 		  $consulta1->execute();
 		  $linha1 = $consulta1->fetch(PDO::FETCH_ASSOC);
 		  $codigo_condominio = $linha1['fk_condominio_codigo_condominio'];
-		
-		  $consulta2 = $db_con->prepare("SELECT codigo_importancia FROM importancia where desc_importancia = '$importancia'");
-		  $consulta2->execute();
-		  $linha2 = $consulta2->fetch(PDO::FETCH_ASSOC);
-		  $codigo_importancia = $linha2['codigo_importancia'];
+		 
+
+  		  
+
+		 $consulta = null;
+		 if ($importancia === "Todos") {
+			$consulta = $db_con->prepare("SELECT * FROM aviso where fk_condominio_codigo_condominio = '$codigo_condominio'");
+		} else {
+			$consulta2 = $db_con->prepare("SELECT codigo_importancia FROM importancia where desc_importancia = '$importancia'");
+		  	$consulta2->execute();
+		  	$linha2 = $consulta2->fetch(PDO::FETCH_ASSOC);
+		 	$codigo_importancia = $linha2['codigo_importancia'];
+			$consulta = $db_con->prepare("SELECT * FROM aviso where fk_condominio_codigo_condominio = '$codigo_condominio' AND fk_importancia_codigo_importancia = '$codigo_importancia'");
+		}
 	
-		$consulta = $db_con->prepare("SELECT * FROM aviso where fk_condominio_codigo_condominio = '$codigo_condominio' AND fk_importancia_codigo_importancia = '$codigo_importancia'");
+		
 	
 		if ($consulta->execute()) {
 	  		while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
