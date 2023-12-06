bb0e6127d8e4da3ca0347bea2abed411d2f8bbac
projintegrador2023 <127967558+projintegrador2023@users.noreply.github.com>
2023-12-06 14:42:42 -0300

    Update pegar_perfil.php

--- html/mobile/pegar_perfil.php
+++ html/mobile/pegar_perfil.php
@@ -25,7 +25,7 @@ if ($consulta->execute()) {
     $perfil["codigo_nivel_permissao"] = $linha["fk_nivel_permissao_codigo_nivel_permissao"];
     $perfil["codigo_condominio"] = $linha["fk_condominio_codigo_condominio"];
     $perfil["senha"] = $linha["senha"];
-   $codigo_moradia = $linha["fk_moradia_codigo_moradia"]
+   $codigo_moradia = $linha["fk_moradia_codigo_moradia"];
     
     $consulta1 = $db_con->prepare("SELECT * FROM moradia where codigo_moradia = '$codigo_moradia'");
     $consulta1->execute();
